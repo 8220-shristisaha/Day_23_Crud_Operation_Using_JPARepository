c0a9a325ded65cc6da5902ccf433ba005c56fc61
Shri <Shristi.Saha@godigit.com>
2024-08-21 17:45:26 +0530

    [Shristi] ADD : Successfully created Entity and Repository

--- src/main/java/com/example/Employee/entity/Employee.java
+++ src/main/java/com/example/Employee/entity/Employee.java
@@ -1,5 +1,23 @@
 package com.example.Employee.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employee_table")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String department;
+    private double salary;
 }
